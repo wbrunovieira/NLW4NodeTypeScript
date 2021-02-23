5e6b82b11f67fb00484337ca0e06070d36421afa
wbrunovieira <wbrunovieira77@gmail.com>
2021-02-23 19:29:28 +0000

    made orm, database, usercontroller, usermodel, route, post create user

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { Router } from 'express';
+import './database';
+import { router } from "./routes";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    return res.json({message:"Hello World - NLW4"})
-})
-
-app.post("/", (req, res) => {
-    return res.json({message: "dados salvos com sucesso !"})
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running !!!!"));
