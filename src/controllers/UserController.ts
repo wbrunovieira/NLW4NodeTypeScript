4422cc0cd8cca01ae2ce3d0e97c91e0e17f0c8e2
wbrunovieira <wbrunovieira77@gmail.com>
2021-02-24 19:31:39 +0000

    refactorede controller, and made new Table, model, Controller, Repository of Survey

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,13 +1,13 @@
-import { Request, Response} from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UserRepositories';
 
 class UserController {
 
     async create(request: Request, response: Response) {
         const { name, email} = request.body;
         
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UsersRepository);
 
         const userAlreadyExists = await usersRepository.findOne({
             email
@@ -27,4 +27,4 @@ class UserController {
     }
 }
 
-export { UserController};
+export { UserController };
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import { Router} from "express";
 import { UserController } from "./controllers/UserController";
+import { SurveyController } from "./controllers/SurveyController";
 
 const router = Router();
 
 const userController = new UserController();
+const surveysController = new SurveyController();
+
 router.post("/users", userController.create);
+router.post("/surveys", surveysController.create)
 export { router };
